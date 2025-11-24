c576e8cb3708b0ec4f6b9370bcc0f61eec0e7f5f
Kieran Llarena <baymax709@gmail.com>
2025-11-23 19:53:39 -0500

    refactor: more robust error handling

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
 use std::{
-    io::{Read, Write},
+    io::{self, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_client(mut stream: TcpStream) {
-    let addr = stream.peer_addr().unwrap();
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    let addr = stream.peer_addr()?;
     println!("Connected: {}", addr);
     let mut buf = [0u8; 4096];
     loop {
-        let n = stream.read(&mut buf).unwrap();
+        let n = stream.read(&mut buf)?;
         if n == 0 {
             println!("Disconnected: {}", addr);
             break;
         }
 
-        std::io::stdout().write_all(&buf[..n]).unwrap();
-        std::io::stdout().flush().unwrap();
+        std::io::stdout().write_all(&buf[..n])?;
+        std::io::stdout().flush()?;
     }
+    Ok(())
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let address = "0.0.0.0:2133";
 
     println!("Binding to port {}", address);
 
-    let listener = TcpListener::bind(address).unwrap();
+    let listener = TcpListener::bind(address)?;
 
-    for stream in listener.incoming() {
-        handle_client(stream.unwrap());
+    for connection in listener.incoming() {
+        match connection {
+            Ok(stream) => {
+                if let Err(err) = handle_client(stream) {
+                    eprintln!("Client handler error: {}", err);
+                }
+            }
+            Err(err) => eprintln!("Accept error: {}", err),
+        }
     }
+
+    Ok(())
 }
